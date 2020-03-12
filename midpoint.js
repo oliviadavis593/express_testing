f3e24ae133639a6eeb86504d447b80bf02395d7b
Olivia Davis <olivia.davis593@gmail.com>
2020-03-12 13:26:36 -0500

    tested calculations of a lat lon midpoint using .all

--- midpoint.js
+++ midpoint.js
@@ -9,38 +9,39 @@ const midpoint = express();
 
 function toRadians(deg) {
     return deg * (Math.PI / 180);
-} 
-
-function toDegrees(rad) {
+  }
+  
+  function toDegrees(rad) {
     return rad * (180 / Math.PI);
-}
-
-midpoint.get('/midpoint', (req, res) => {
-    const { lat1, lon1, lat2, lon2 } = req.query; 
-
-    //for vrevity the validation is skipped 
-
-    //convert to radians 
+  }
+  
+  midpoint.get('/midpoint', (req, res) => {
+    const { lat1, lon1, lat2, lon2 } = req.query;
+  
+    // for brevity the validation is skipped
+  
+    // convert to radians
     const rlat1 = toRadians(lat1);
     const rlon1 = toRadians(lon1);
     const rlat2 = toRadians(lat2);
     const rlon2 = toRadians(lon2);
-
-    const bx = Math.cos(rlat2) * Math.cost(rlon2 - rlon1);
+  
+    const bx = Math.cos(rlat2) * Math.cos(rlon2 - rlon1);
     const by = Math.cos(rlat2) * Math.sin(rlon2 - rlon1);
-
+  
     const midLat = Math.atan2(
-        Math.sin(rlat1) + Math.sin(rlat2),
-        Math.sqrt(
-            (Math.cos(rlat1) + bx)
-            * (Math.cos(rlat1) + bx)
-            + by * by
-        )
+      Math.sin(rlat1) + Math.sin(rlat2),
+      Math.sqrt(
+        (Math.cos(rlat1) + bx)
+        * (Math.cos(rlat1) + bx)
+        + by * by
+      )
     );
-    const midLon = rlon1 + Math.atan2(by, Math.cost(rlat1) + bx);
-    
+    const midLon = rlon1 + Math.atan2(by, Math.cos(rlat1) + bx);
+  
     res.json({
-        lat: toDegrees(midLat),
-        lon: toDegrees(midLon),
+      lat: toDegrees(midLat),
+      lon: toDegrees(midLon)
     })
-})
+  });
+module.exports = midpoint; 
