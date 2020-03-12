9a986876f3d1a999c0baf28e27bc2bffc3e56426
Olivia Davis <olivia.davis593@gmail.com>
2020-03-12 12:50:02 -0500

    tested calculations of a lat lon midpoint using .all

---
+++ midpoint.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const midpoint = express();
+
+//Write an endpoint handler function GET /midpoint that accepts lat & long coordinates
+//For two points on the Earth 
+//Calculate the midpoint between these 2 points & return an object
+// { lat: midLat, lon: midLon }
+
+function toRadians(deg) {
+    return deg * (Math.PI / 180);
+} 
+
+function toDegrees(rad) {
+    return rad * (180 / Math.PI);
+}
+
+midpoint.get('/midpoint', (req, res) => {
+    const { lat1, lon1, lat2, lon2 } = req.query; 
+
+    //for vrevity the validation is skipped 
+
+    //convert to radians 
+    const rlat1 = toRadians(lat1);
+    const rlon1 = toRadians(lon1);
+    const rlat2 = toRadians(lat2);
+    const rlon2 = toRadians(lon2);
+
+    const bx = Math.cos(rlat2) * Math.cost(rlon2 - rlon1);
+    const by = Math.cos(rlat2) * Math.sin(rlon2 - rlon1);
+
+    const midLat = Math.atan2(
+        Math.sin(rlat1) + Math.sin(rlat2),
+        Math.sqrt(
+            (Math.cos(rlat1) + bx)
+            * (Math.cos(rlat1) + bx)
+            + by * by
+        )
+    );
+    const midLon = rlon1 + Math.atan2(by, Math.cost(rlat1) + bx);
+    
+    res.json({
+        lat: toDegrees(midLat),
+        lon: toDegrees(midLon);
+    })
+})
